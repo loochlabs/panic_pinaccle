68fbd69f17846c150de6a634e40f55c3a8cfe9c4
Steve Coluccio <thecalooch@gmail.com>
2018-02-06 11:28:11 -0500

    Initial Unity Project

---
+++ tower_climb/Assets/scripts/CameraControls.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControls : MonoBehaviour {
+
+    public Transform boundsCenter;
+    public Vector3 offset = new Vector3(0,0,-10f); 
+    
+	void Update () {
+        transform.position = boundsCenter.position + offset;
+	}
+}
