06e22d3a5804ccd636b0316e95a2b852054d17d1
Chris L <clindgren111@gmail.com>
2018-02-08 21:42:18 -0500

    Added basic implementation for certain behaviors on AudioManager/DataManager. Added script for GlobalAssetData.

---
+++ tower_climb/Assets/_PanicPinnacle/_Scripts/Debugging/AudioTester.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Sirenix.OdinInspector;
+
+namespace PanicPinnacle.Debugging {
+
+	/// <summary>
+	/// A sample class to make sure that the AudioManager is working as intended.
+	/// </summary>
+	public class AudioTester : SerializedMonoBehaviour {
+
+		#region FIELDS
+		/// <summary>
+		/// The type of SFX to test out.
+		/// </summary>
+		[SerializeField]
+		private SFXType sfxType = SFXType.MenuHover;
+		[SerializeField]
+		private float volumeScale = 1f;
+		#endregion
+
+		/// <summary>
+		/// Test to make sure the AudioManager is working properly.
+		/// </summary>
+		[SerializeField]
+		public void TestSFX() {
+			AudioManager.instance.PlaySFX(type: this.sfxType, volumeScale: this.volumeScale);
+		}
+
+	}
+
+
+}
---
+++ tower_climb/Assets/_PanicPinnacle/_Scripts/Enums/SFXType.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PanicPinnacle {
+
+	/// <summary>
+	/// The different types of SFX there are.
+	/// Used for SFX that are common enough to warrant assigning them a type.
+	/// </summary>
+	public enum SFXType {
+		MenuHover = 1001,		
+		MenuSelect = 1002,
+		Punch1 = 2001,
+		Punch2 = 2002,
+		Jump1 = 2101,
+		ERROR = 9999,
+	}
+
+
+}
