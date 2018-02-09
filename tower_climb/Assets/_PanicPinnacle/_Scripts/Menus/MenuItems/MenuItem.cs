9d29ca3ce34f048854b8e6712f859a3b28385296
Chris L <clindgren111@gmail.com>
2018-02-09 14:05:12 -0500

    Work on MenuItems

---
+++ tower_climb/Assets/_PanicPinnacle/_Scripts/Menus/MenuItems/MenuItem.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using Sirenix.OdinInspector;
+
+namespace PanicPinnacle.UI {
+
+	/// <summary>
+	/// The way in which items in menus should be implemented.
+	/// </summary>
+	[RequireComponent(typeof(Selectable))]
+	public abstract class MenuItem : SerializedMonoBehaviour, ICancelHandler, ISelectHandler, ISubmitHandler, IDeselectHandler, IPointerEnterHandler, IPointerExitHandler {
+
+		#region INTERFACE IMPLEMENTATION - THE EVENTSYSTEM JUNK
+		public abstract void OnCancel(BaseEventData eventData);
+		public abstract void OnDeselect(BaseEventData eventData);
+		public abstract void OnPointerEnter(PointerEventData eventData);
+		public abstract void OnPointerExit(PointerEventData eventData);
+		public abstract void OnSelect(BaseEventData eventData);
+		public abstract void OnSubmit(BaseEventData eventData);
+		#endregion
+
+	}
+}
