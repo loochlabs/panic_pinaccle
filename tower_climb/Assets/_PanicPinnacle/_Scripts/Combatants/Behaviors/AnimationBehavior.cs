68b7f814f203bd003b46dd55ec29b311fefcc6e7
Steve Coluccio <thecalooch@gmail.com>
2018-02-22 23:08:31 -0500

    player ph anims, particle systems; behaviors for each

---
+++ tower_climb/Assets/_PanicPinnacle/_Scripts/Combatants/Behaviors/AnimationBehavior.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PanicPinnacle.Events;
+using PanicPinnacle.Combatants.Behaviors;
+using PanicPinnacle.Combatants;
+
+public class AnimationBehavior : CombatantBehavior, IUpdateEvent
+{
+
+    private Animator anim;
+    private CombatantStateType prevState;
+
+    #region INTERFACES METHODS
+    public override void Prepare(Combatant combatant)
+    {
+        anim = combatant.GetComponentInChildren<Animator>();
+    }
+
+    public void Update(CombatantEventParams eventParams)
+    {
+        //player running
+        Vector3 inputDirection =
+            eventParams.combatant.CombatantInput.GetMovementDirection(combatant: eventParams.combatant);
+        anim.SetBool("running", inputDirection.magnitude > 0);
+        
+        //state specific anims
+        if (prevState != eventParams.combatant.State) {
+            switch (eventParams.combatant.State)
+            {
+                case CombatantStateType.playing:
+                    anim.SetBool("dazed", false);
+                    anim.SetBool("punching", false);
+                    break;
+                case CombatantStateType.dazed:
+                    anim.SetBool("dazed", true);
+                    anim.SetBool("punching", false);
+                    break;
+                case CombatantStateType.punching:
+                    anim.SetBool("punching", true);
+                    anim.SetBool("dazed", false);
+                    break;
+            }
+        }
+        prevState = eventParams.combatant.State;
+    }
+
+    #endregion
+
+    #region INSPECTOR JUNK
+    private static string inspectorDescription = "The behavior that controls combatant animations.";
+    protected override string InspectorDescription
+    {
+        get
+        {
+            return inspectorDescription;
+        }
+    }
+    #endregion
+}
---
+++ tower_climb/Assets/_PanicPinnacle/_Scripts/Combatants/Behaviors/ParticleBehavior.cs
@@ -0,0 +1,68 @@
+﻿using PanicPinnacle.Combatants;
+using PanicPinnacle.Combatants.Behaviors;
+using PanicPinnacle.Events;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleBehavior : CombatantBehavior, IUpdateEvent
+{
+
+    private ParticleSystem dazedParticles;
+    private ParticleSystem punchingParticles;
+    private CombatantStateType prevState;
+
+    public override void Prepare(Combatant combatant)
+    {
+        //grab the particle systems on our Combatant
+        //@TODO: not sure if this is the best way but it keeps from having to slap references on the inspector.
+        ParticleSystem[] particles = combatant.GetComponentsInChildren<ParticleSystem>();
+        foreach(ParticleSystem ps in particles)
+        {
+            if (ps.gameObject.name == "Dazed Particles") { dazedParticles = ps; }
+            if (ps.gameObject.name == "Punching Particles") { punchingParticles = ps; }
+        }
+
+        if (dazedParticles) {
+            dazedParticles.Stop();
+        }
+        if (punchingParticles)
+        {
+            punchingParticles.Stop();
+        }
+    }
+
+    public void Update(CombatantEventParams eventParams)
+    {
+        if(prevState != eventParams.combatant.State)
+        {
+            switch (eventParams.combatant.State) {
+                case CombatantStateType.dazed:
+                    dazedParticles.Play();
+                    punchingParticles.Stop();
+                    break;
+                case CombatantStateType.punching:
+                    punchingParticles.Play();
+                    dazedParticles.Stop();
+                    break;
+                default:
+                    punchingParticles.Stop();
+                    dazedParticles.Stop();
+                    break;
+            }
+        }
+        prevState = eventParams.combatant.State;
+    }
+
+
+    #region INSPECTOR JUNK
+    private static string inspectorDescription = "The behavior that controls combatant animations.";
+    protected override string InspectorDescription
+    {
+        get
+        {
+            return inspectorDescription;
+        }
+    }
+    #endregion
+}
